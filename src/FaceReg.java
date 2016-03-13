7856ef38054fe13f7923f3a78714418521b89fd2
Gravituss <zaretskyyy@gmail.com>
2016-03-13 22:46:55 +0200

    btw commit 08

--- src/FaceReg.java
+++ src/FaceReg.java
@@ -11,45 +11,45 @@ public class FaceReg {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://facebook.com");
 		driver.manage().window().maximize();
-		
+
 		// preparing data for input (preferably change name and email, please)
 		String firstName = "Gregory";
 		String lastName = "Wondershultz";
 		String emailOrPhone = "x4fzpors0kl364z5f@gmail.com";
 		String newPass = "45sd91oj";
-		
-		//Birthday
+
+		// Birthday
 		String month3Letters = "May";
 		String day = "22";
 		String year = "1985";
 		boolean female = false;
-		
-		//entering text data
+
+		// entering text data
 		driver.findElement(By.id("u_0_1")).sendKeys(firstName);
 		driver.findElement(By.id("u_0_3")).sendKeys(lastName);
 		driver.findElement(By.id("u_0_6")).sendKeys(emailOrPhone);
 		driver.findElement(By.id("u_0_9")).sendKeys(emailOrPhone);
 		driver.findElement(By.id("u_0_b")).sendKeys(newPass);
-		
-		//entering birthday into dropdown menus
-		
+
+		// entering birthday into dropdown menus
+
 		Select dropdown = new Select(driver.findElement(By.id("month")));
-		dropdown.selectByVisibleText(month3Letters);		
+		dropdown.selectByVisibleText(month3Letters);
 		Select dropdown2 = new Select(driver.findElement(By.id("day")));
-		dropdown2.selectByValue(day);		
+		dropdown2.selectByValue(day);
 		Select dropdown3 = new Select(driver.findElement(By.id("year")));
-		dropdown3.selectByVisibleText(year);	
-		
+		dropdown3.selectByVisibleText(year);
+
 		// selecting gender depending of input data
-		if (female){driver.findElement(By.id("u_0_e")).click();} 
-			else {driver.findElement(By.id("u_0_f")).click();}
-		
-		//clicking Sign Up button
+		if (female) {
+			driver.findElement(By.id("u_0_e")).click();
+		} else {
+			driver.findElement(By.id("u_0_f")).click();
+		}
+
+		// clicking Sign Up button
 		driver.findElement(By.id("u_0_j")).click();
-		
-		
 
 	}
-	
 
 }
